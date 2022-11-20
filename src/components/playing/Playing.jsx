6461d51ab75a30adab456e4de5c0f11f0adebea4
giathai1505 <thai.ngo@usumsoftware.com>
2022-11-20 19:30:10 +0700

    add sound function

--- src/components/playing/Playing.jsx
+++ src/components/playing/Playing.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./playing.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,75 +11,148 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 const cx = classNames.bind(styles);
 
-export default function Playing() {
+export default function Playing({ song, onNext, onPrevious }) {
   const [iconChange, setIconChange] = useState(faCirclePlay);
-  const handlePlaying = (e) => {
+  const [timePercent, setTimePercent] = useState(0);
+  const audioRef = useRef();
+  const progressBarRef = useRef();
+  const [duration, setDuration] = useState("");
+  const [curTime, setCurTime] = useState("");
+
+  useEffect(() => {
+    onReset(song?.file);
+  }, [song]);
+
+  const handlePlaying = () => {
     if (iconChange === faCirclePlay) {
       setIconChange(faPauseCircle);
+      audioRef.current.play();
     }
     if (iconChange === faPauseCircle) {
       setIconChange(faCirclePlay);
+      audioRef.current.pause();
     }
+  };
+
+  const handleUpdateProgressBar = () => {
+    const duration = audioRef.current.duration;
+    const currentTime = audioRef.current.currentTime;
+
+    const timePercent = Math.floor((currentTime / duration) * 100);
+    setTimePercent(timePercent);
+    renderCurTime();
+  };
+
+  const handleEndAudio = () => {
+    onNext();
+  };
+
+  const handleClickProgressBar = (e) => {
+    const width = progressBarRef.current.clientWidth;
+    const offset = e.nativeEvent.offsetX;
+
+    audioRef.current.currentTime = (offset / width) * audioRef.current.duration;
+    setTimePercent(Math.floor(offset / width) * 100);
+  };
+
+  const onReset = (src) => {
+    if (audioRef.current) {
+      audioRef.current.src = src;
+
+      progressBarRef.current.value = 0;
+      setTimePercent(0);
+      setIconChange(faCirclePlay);
+      handlePlaying();
+      renderTime();
+    }
+  };
+
+  const renderTime = () => {
+    const minutes = Math.floor(Math.floor(song.duration) / 60);
+    const second = Math.floor(song.duration) % 60;
+    setDuration(minutes + ":" + second);
+  };
+
+  const renderCurTime = () => {
+    const minutes = Math.floor(Math.floor(audioRef.current.currentTime) / 60);
+    const second = Math.floor(audioRef.current.currentTime) % 60;
+    const formatSecond = ("0" + second).slice(0, 2);
+    setCurTime(minutes + ":" + formatSecond);
   };
   return (
     <div className={cx("playing-song")}>
-      <div className={cx("player-box")}>
-        <div className={cx("player-media")}>
-          <img
-            src={require("./../../assets/images/image.png")}
-            alt=""
-            className={cx("player-image")}
-          />
-        </div>
-        <h3 className={cx("song-title")}>End of time</h3>
-        <h3 className={cx("song-desc")}>K-391, Alan Walker & Ahrix</h3>
-        <p className={cx("song-lyric")}>No, lyric</p>
-        <div className={cx("progress")}>
-          <input
-            type="range"
-            id="progress-bar"
-            min="0"
-            max=""
-            value="0"
-            className={cx("bar")}
-          />
-          <audio src="./files/holo.mp3" id="song"></audio>
-        </div>
-        <div className={cx("player-number")}>
-          <span className={cx("player-remaining")}>0:00</span>
-          <td className="d-flex">
-            <button class={cx("repeat", "btn-action")}>
-              <FontAwesomeIcon
-                icon={faRepeat}
-                className={cx("icon")}
-              ></FontAwesomeIcon>
-            </button>
+      {song ? (
+        <>
+          <audio
+            controls="controls"
+            ref={audioRef}
+            className="hidden"
+            onTimeUpdate={handleUpdateProgressBar}
+            onEnded={handleEndAudio}
+          >
+            <source src={song.file} type="audio/mpeg" />
+          </audio>
 
-            <button class={cx("back", "btn-action")}>
-              <FontAwesomeIcon
-                icon={faBackwardStep}
-                className={cx("icon")}
-              ></FontAwesomeIcon>
-            </button>
-            <button class={cx("play-song", "btn-action")}>
-              <FontAwesomeIcon
-                icon={iconChange}
-                className={cx("icon")}
-                onClick={handlePlaying}
-              ></FontAwesomeIcon>
-            </button>
-            <button class={cx("forward", "btn-action")}>
-              <FontAwesomeIcon
-                icon={faForwardStep}
-                className={cx("icon")}
-              ></FontAwesomeIcon>
-            </button>
-          </td>
-          <span className={cx("player-duration")}>0:00</span>
-        </div>
-      </div>
+          <div className={cx("player-box")}>
+            <div className={cx("player-media")}>
+              <img src={song.image} alt="" className={cx("player-image")} />
+            </div>
+            <h3 className={cx("song-title")}>{song.name}</h3>
+            <h3 className={cx("song-desc")}>{song.artist}</h3>
+            <p className={cx("song-lyric")}>No, lyric</p>
+            <div className={cx("progress")}>
+              <input
+                type="range"
+                id="progress-bar"
+                min="0"
+                max="100"
+                ref={progressBarRef}
+                value={timePercent || 0}
+                className={cx("bar")}
+                onClick={(e) => handleClickProgressBar(e)}
+              />
+            </div>
+            <div className={cx("player-number")}>
+              <span className="text-white">{curTime ? curTime : "0:00"}</span>
+              <td className="flex text-white gap-3">
+                <button class={cx("repeat", "btn-action")}>
+                  <FontAwesomeIcon
+                    icon={faRepeat}
+                    className={cx("icon")}
+                  ></FontAwesomeIcon>
+                </button>
 
-      <div className={cx("player-tool")}></div>
+                <button class={cx("back", "btn-action")}>
+                  <FontAwesomeIcon
+                    icon={faBackwardStep}
+                    className={cx("icon")}
+                    onClick={onPrevious}
+                  ></FontAwesomeIcon>
+                </button>
+                <button class={cx("play-song", "btn-action")}>
+                  <FontAwesomeIcon
+                    icon={iconChange}
+                    className={cx("icon")}
+                    onClick={handlePlaying}
+                  ></FontAwesomeIcon>
+                </button>
+                <button class={cx("forward", "btn-action")}>
+                  <FontAwesomeIcon
+                    icon={faForwardStep}
+                    className={cx("icon")}
+                    onClick={onNext}
+                  ></FontAwesomeIcon>
+                </button>
+              </td>
+              <span className="text-white">{duration ? duration : "0:00"}</span>
+            </div>
+          </div>
+
+          <div className={cx("player-tool")}></div>
+        </>
+      ) : (
+        "loading"
+      )}
     </div>
   );
 }
--- src/layout/Home/Home.jsx
+++ src/layout/Home/Home.jsx
@@ -1,31 +1,80 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./home.module.scss";
-import { Space, Table } from "antd";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHeart as heartSolid,
-  faPlay,
-} from "@fortawesome/free-solid-svg-icons";
-import { faHeart as heartregular } from "@fortawesome/free-regular-svg-icons";
-import Modal from "../../components/modal/Modal";
+import { Table } from "antd";
+import axios from "axios";
+import { BsTrash } from "react-icons/bs";
+import { BiEdit } from "react-icons/bi";
+import Playing from "../../components/playing/Playing";
+
 const cx = classNames.bind(styles);
 
 export default function Home() {
-  const [iconChange, setIconChange] = useState(heartregular);
-  const handleFavorite = () => {
-    if (iconChange === heartSolid) {
-      setIconChange(heartregular);
-    }
-    if (iconChange === heartregular) {
-      setIconChange(heartSolid);
+  const [iconChange, setIconChange] = useState();
+  const [listMusics, setListMusics] = useState([]);
+  const [selectedSong, setSelectedSong] = useState();
+
+  const handleMoveNext = () => {
+    let curIndex = listMusics.findIndex(
+      (item) => item._id === selectedSong._id
+    );
+    curIndex = curIndex === listMusics.length - 1 ? 0 : ++curIndex;
+    setSelectedSong(listMusics[curIndex]);
+  };
+
+  const handleMovePrevious = () => {
+    console.log("Previous");
+    let curIndex = listMusics.findIndex(
+      (item) => item._id === selectedSong._id
+    );
+    curIndex = curIndex === 0 ? listMusics.length - 1 : --curIndex;
+    setSelectedSong(listMusics[curIndex]);
+  };
+
+  const getAllMusicsAPI = async () => {
+    try {
+      const result = await axios.get("http://localhost:5000/api/sound");
+
+      if (result.data.sounds) {
+        setListMusics(result.data.sounds);
+      }
+    } catch (error) {
+      console.log("login error:", error);
     }
   };
+
+  useEffect(() => {
+    getAllMusicsAPI();
+  }, []);
+
+  useEffect(() => {
+    setSelectedSong(listMusics[listMusics.length - 1]);
+  }, [listMusics]);
+
+  console.log(selectedSong);
+
   const columns = [
     {
+      title: "#",
+      dataIndex: "stt",
+      key: "stt",
+      className: "sttRow",
+      render: (text, record, index) => (
+        <div className="flex items-center gap-3  justify-end">
+          {selectedSong &&
+            (index ===
+            listMusics.findIndex((item) => item?._id === selectedSong?._id) ? (
+              <img src="https://stc-id.nixcdn.com/v11/images/icon_status.gif" />
+            ) : null)}
+
+          <span>{++index}</span>
+        </div>
+      ),
+    },
+    {
       title: "Title",
-      dataIndex: "title",
-      key: "title",
+      dataIndex: "name",
+      key: "name",
     },
     {
       title: "Artist",
@@ -34,66 +83,42 @@ export default function Home() {
     },
     {
       title: "Time",
-      dataIndex: "time",
-      key: "time",
+      dataIndex: "duration",
+      key: "duration",
     },
     {
       title: "Action",
       dataIndex: "Action",
       key: "Action",
+      className: "actionRow",
 
       render: (text, record, index) => (
-        <Space size="middle">
-          <div className={cx("")}>
-            <FontAwesomeIcon
-              icon={faPlay}
-              className={cx("icon")}
-            ></FontAwesomeIcon>
-          </div>
-          <div className={cx("")}>
-            <FontAwesomeIcon
-              icon={iconChange}
-              className={cx("icon-heart")}
-              onClick={handleFavorite}
-            ></FontAwesomeIcon>
-          </div>
-        </Space>
+        <div className="flex items-center gap-2">
+          <BsTrash
+            className="cursor-pointer hover:scale-150"
+            onClick={() => console.log()}
+          />
+          <BiEdit
+            className="cursor-pointer hover:scale-150"
+            onClick={() => console.log()}
+          />
+        </div>
       ),
-    },
-  ];
-  const data = [
-    {
-      key: "1",
-      title: "The sound of a stream flowing",
-      artist: "Relaxing River Sounds",
-      time: "3:00",
-    },
-    {
-      key: "2",
-      title: "The sound of a stream flowing",
-      artist: "Hihi",
-      time: "3:15",
-    },
-    {
-      key: "4",
-      title: "The sound of a stream flowing",
-      artist: "Relaxing River Sounds",
-      time: "3:00",
-    },
-    {
-      key: "5",
-      title: "The sound of a stream flowing",
-      artist: "Hihi",
-      time: "3:15",
     },
   ];
 
   return (
-    <div className={cx("home-container")}>
-      <Modal></Modal>
-      <h1 className={cx("title")}>PlayList</h1>
-      <div className={cx("content")}>
-        <Table columns={columns} dataSource={data} className={cx("table")} />
+    <div className="grid grid-cols-3">
+      <div className="col-span-2">
+        <h3 className="font-semibold text-white text-xl">LIST MUSIC</h3>
+        <Table columns={columns} dataSource={listMusics} />
+      </div>
+      <div className="col-span-1">
+        <Playing
+          song={selectedSong}
+          onNext={handleMoveNext}
+          onPrevious={handleMovePrevious}
+        />
       </div>
     </div>
   );
